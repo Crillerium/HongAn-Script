1089fdc6b8b485cfff51b27245f6428f47e6274c
Crillerium <96243292+Crillerium@users.noreply.github.com>
2022-07-13 12:17:46 +0800

    Update script.php

--- script.php
+++ script.php
@@ -3,22 +3,21 @@ function info(){
 $info = <<<info
 脚本名称:HongAn Script<br/>
 脚本作者:Crillerium<br/>
-版本:v1.0
 info;
 return $info;
 }
 function core($string){
-$md5=md5($string);
+$base64=base64_encode($string);
 $sha1=sha1($string);
-$md5sha1 = md5($md5.$sha1);
-$result = substr($md5sha1,1,17);
+$md5 = md5($base64.$sha1);
+$result = substr($md5,1,17);
 return $result;
 }
 function match($string,$secret){
-$md5=md5($string);
+$base64=base64_encode($string);
 $sha1=sha1($string);
-$md5sha1 = md5($md5.$sha1);
-$result = substr($md5sha1,1,17);
+$md5 = md5($base64.$sha1);
+$result = substr($md5,1,17);
 if($result==$secret){
 return "true";
 }
